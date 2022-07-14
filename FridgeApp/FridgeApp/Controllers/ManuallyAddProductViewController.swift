73df224040ae9efbee7a5485201a0002d61503d7
Khushneet <khushneet.inder.singh@live.com>
2022-07-15 01:28:44 +0530

    Added few view controllers for scanner.

---
+++ FridgeApp/FridgeApp/Controllers/ManuallyAddProductViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ManuallyAddProductViewController.swift
+//  FridgeApp
+//
+//  Created by Khushneet on 15/07/22.
+//
+
+import UIKit
+
+class ManuallyAddProductViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
